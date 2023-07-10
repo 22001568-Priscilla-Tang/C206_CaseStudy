ff4a44c905eb0afe8187579078c6cbc718353ea3
22001568-Priscilla-Tang <134248150+22001568-Priscilla-Tang@users.noreply.github.com>
2023-07-10 12:00:31 +0800

    delete comment

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -15,7 +15,7 @@ public class C206_CaseStudyTest {
 	}
 
 	@Test
-	public void c206_test() {//gfgfgffgf
+	public void c206_test() {
 		//fail("Not yet implemented"); 
 		assertTrue("C206_CaseStudy_SampleTest ",true);
 	}
